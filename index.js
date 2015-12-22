70aca028fdf3ba87c947299423a326ed7f3f00c7
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-22 18:31:32 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
+var Linkedin = require('node-linkedin')(),
+    _ = require('lodash'),
+    util = require('./util.js');
+
+var pickInputs = {
+        'id': 'id',
+        'comment': 'comment',
+        'content_title': 'content.title',
+        'content_description': 'content.description',
+        'content_url': 'content.submitted-url',
+        'content_image_url': 'content.submitted-image-url',
+        'visibility_code': 'visibility.code'
+    },
+    pickOutputs = {
+        'updateKey': 'updateKey',
+        'updateUrl': 'updateUrl'
+    };
+
 module.exports = {
+
+    /**
+     * Authorize module.
+     *
+     * @param dexter
+     * @returns {*}
+     */
+    authModule: function (dexter) {
+        var accessToken = dexter.environment('linkedin_access_token');
+
+        if (accessToken)
+            return Linkedin.init(accessToken);
+
+        else
+            return false;
+    },
+
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +42,22 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var linkedIn = this.authModule(dexter),
+            inputs = util.pickStringInputs(step, pickInputs);
+
+        if (!linkedIn)
+            return this.fail('A [linkedin_access_token] environment need for this module.');
+
+        linkedIn.companies.share(inputs.id, _.omit(inputs, ['id']), function(err, data) {
+            if (err)
+                this.fail(err);
+
+            else if (data.errorCode !== undefined)
+                this.fail(data.message || 'Error Code'.concat(data.errorCode));
+
+            else
+                this.complete(util.pickResult(data, pickOutputs));
+
+        }.bind(this));
     }
 };
