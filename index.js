94b97bfbe4a4bee5e0e7aea351ebd549e905b85f
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-25 12:23:29 +0200

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Linkedin = require('node-linkedin')(),
     util = require('./util.js');
 
 var pickInputs = {
-        'id': 'id',
+        'id': { key: 'id', validate: { req: true } },
         'comment': 'comment',
         'content_title': 'content.title',
         'content_description': 'content.description',
@@ -17,24 +17,6 @@ var pickInputs = {
     };
 
 module.exports = {
-
-    /**
-     * Authorize module.
-     *
-     * @param dexter
-     * @returns {*}
-     */
-    authModule: function (dexter) {
-        var accessToken = dexter.environment('linkedin_access_token');
-
-        if (accessToken)
-            return Linkedin.init(accessToken);
-
-        else
-            return false;
-    },
-
-
     /**
      * The main entry point for the Dexter module
      *
@@ -42,24 +24,18 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var linkedIn = this.authModule(dexter),
-            inputs = util.pickStringInputs(step, pickInputs);
+        var linkedIn = Linkedin.init(dexter.provider('linkedin').credentials('access_token')),
+            inputs = util.pickStringInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
 
-        if (!linkedIn)
-            return this.fail('A [linkedin_access_token] environment need for this module.');
-
-        if (!inputs.id)
-            this.fail('A [id] need for this module');
+        if (validateErrors)
+            return this.fail(validateErrors);
 
         linkedIn.companies.share(inputs.id, _.omit(inputs, ['id']), function(err, data) {
-            if (err)
-                this.fail(err);
-
-            else if (data.errorCode !== undefined)
-                this.fail(data.message || 'Error Code'.concat(data.errorCode));
-
+            if (err || (data && data.errorCode !== undefined))
+                this.fail(err || (data.message || 'Error Code: '.concat(data.errorCode)));
             else
-                this.complete(util.pickResult(data, pickOutputs));
+                this.complete(util.pickOutputs(data, pickOutputs));
 
         }.bind(this));
     }
